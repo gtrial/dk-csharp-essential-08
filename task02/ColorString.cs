b7b063da0bc878419b3811f693f3071ef3c920ea
gtrial <gtrial@users.noreply.github.com>
2021-04-28 19:28:38 +0300

    Task 02 completed

---
+++ task02/ColorString.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace task02
+{
+    public static class ColorString
+    {
+        private enum Colors
+        {
+            Red,
+            Green,
+            Blue
+        }
+
+        public static void Print(string inputString, int color)
+        {
+            switch (color)
+            {
+                case (int)Colors.Red: { Console.ForegroundColor = ConsoleColor.Red; break; }
+                case (int)Colors.Green: { Console.ForegroundColor = ConsoleColor.Green; break; }
+                case (int)Colors.Blue: { Console.ForegroundColor = ConsoleColor.Blue; break; }
+                default:
+                    break;
+            }
+            Console.WriteLine(inputString);
+            Console.ResetColor();
+        }
+    }
+}
